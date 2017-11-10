989cd19a6db24db9594c51b4e165f0ea1f927fef
sqaz91819 <z390124578@gmail.com>
2017-11-11 04:44:53 +0900

    articles score modify interface
    articles score modify interface and CSS files update.

--- mysite/rate/urls.py
+++ mysite/rate/urls.py
@@ -6,4 +6,7 @@ app_name = 'rate'
 urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^index/$', views.IndexView.as_view(), name='index'),
+    url(r'^forms/$', views.FormsView.as_view(), name='forms'),
+    url(r'^modify/$', views.ModifyView.as_view(), name='modify'),
+    url(r'^score/$', views.score, name='score'),
 ]
--- mysite/rate/views.py
+++ mysite/rate/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
-from .models import Query
+from .models import Query, ScoreMovie, Score
 from .crawler_api import mongodb
 # Create your views here.
 
@@ -66,3 +66,49 @@ class IndexView(generic.ListView):
             'form': form,
             'error_message': 'Please enter movie name',
         })
+
+
+class FormsView(generic.TemplateView):
+    template_name = 'rate/forms.html'
+
+
+class ModifyView(generic.ListView):
+    model = ScoreMovie
+    form_class = ScoreMovie
+    template_name = 'rate/modify.html'
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            score = form.score['score2']
+            movie = form.score['movie2']
+            with mongodb.Mongodb(hash_check=False) as mgd:
+                article: dict = mgd.search_dual('articles', 'title', movie, 'score', int(score))
+            try:
+                article['content'] = article['content'].replace(' ', '\n')
+                article['id0'] = article['_id']
+                return render(request, self.template_name, article)
+            except IndexError:
+                return render(request, 'rate/labelfix.html', {'error_message': '資料庫無此電影或沒有該範圍分數的文章'})
+            except TypeError:
+                return render(request, 'rate/labelfix.html', {'error_message': '資料庫無此電影或沒有該範圍分數的文章'})
+
+        return render(request, 'rate/labelfix.html', {'error_message': 'Not valid input.'})
+
+
+def score(request):
+    if request.method == 'POST':
+        form = Score(request.POST)
+
+        if form.is_valid():
+            id1 = form.id1['id1']
+            get_score = form.id1['score1']
+
+            with mongodb.Mongodb(hash_check=False) as mgd:
+                mgd.update_score('articles', id1, int(get_score))
+                # mgd.update_score('jie_ba_Articles', _id, get_score)
+                mgd.update_modified(id1)
+
+            return render(request, 'rate/forms.html')
+
+        return render(request, 'rate/home.html', {'form': form, })
