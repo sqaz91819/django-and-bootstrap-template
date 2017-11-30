8ee55be358cc037d237329a0bf1232a18fe38417
sqaz91819 <z390124578@gmail.com>
2017-11-30 14:26:15 +0900

    Root to Rate
    Root to Rate.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^', include('rate.urls')),
     url(r'^rate/', include('rate.urls')),
     url(r'^admin/', admin.site.urls),
 ]
